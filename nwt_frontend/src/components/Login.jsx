2d35a7968c9c526829d2390573b633061bd60fef
Lucija Brnas <lbrnas00@fesb.hr>
2022-08-17 13:58:01 +0200

    Setting frontend

---
+++ nwt_frontend/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import GoogleLogin from '@react-oauth/google';
+import {useNavigate} from 'react-router-dom';
+import {FcGoogle} from 'react-icons/fc' ;
+
+const Login = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default Login
